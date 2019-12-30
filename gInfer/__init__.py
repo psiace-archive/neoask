ac5eebf7d8aae3c6321a8eb206ab303e8b1e609e
Chojan Shang <psiace@outlook.com>
2019-12-31 00:00:27 +0800

    :tada: Setup basic python envrionment.

---
+++ gInfer/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for gInfer."""
+
+__author__ = """Chojan Shang"""
+__email__ = "psiace@outlook.com"
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gInfer",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
