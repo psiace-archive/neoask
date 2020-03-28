79826237dc3e215ac48f3eada6c54f849b59c86a
Chojan Shang <psiace@outlook.com>
2020-03-28 16:49:47 +0800

    :fire: Chang tech stacks.

--- docs/src/SUMMARY.md
+++
@@ -1,3 +0,0 @@
-# Summary
-
-[gInfer](./intro.md)
--- gInfer/api/routes/api.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from gInfer.api.routes import neo4j
-
-router = APIRouter()
-router.include_router(neo4j.router, tags=["neo4j"], prefix="/neo4j")
